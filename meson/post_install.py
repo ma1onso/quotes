ac742b52ce097ee9f1a9866c5fac1df2c45045ca
Nathan Bonnemains <bonnemainsnathan@gmail.com>
2019-06-10 12:11:47 +0200

    Fix new line warnings

--- meson/post_install.py
+++ meson/post_install.py
@@ -12,4 +12,3 @@ if not environ.get('DESTDIR'):
     subprocess.call(['glib-compile-schemas', schemadir])
     print('Updating icon cache…')
     subprocess.call(['gtk-update-icon-cache', '-qtf', path.join(datadir, 'icons', 'hicolor')])
-    
